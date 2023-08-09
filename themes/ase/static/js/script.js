f65a8796a8ffe4b36d1fe91d1078c988f5ae1329
rizqy110 <aldinmodo@gmail.com>
2023-08-09 07:38:05 +0700

    update

--- themes/ase/static/js/script.js
+++ themes/ase/static/js/script.js
@@ -17,5 +17,7 @@ function getUrlParam(parameter, defaultvalue){
 var arc = getUrlParam('arc',null);
 var lh3 = getUrlParam('lh3',null);
 
-var astream = "<video id='plyr' class='p-a w-f' autoplay='' controls='' playsinline='' webkit-playsinline='' src='https://archive.org/download/"+ arc +".mp4' type='video/mp4'></Video><span class='tl p-a bc1 c1 p-5 br-11'>Donghua Indo</span>";
+var encryptarc = decodeURIComponent(arc);
+
+var astream = "<video id='plyr' class='p-a w-f' autoplay='' controls='' playsinline='' webkit-playsinline='' src='https://archive.org/download/"+ encryptarc +".mp4' type='video/mp4'></Video><span class='tl p-a bc1 c1 p-5 br-11'>Donghua Indo</span>";
 var lh3stream = "<video id='plyr' class='p-a w-f' autoplay='' controls='' playsinline='' webkit-playsinline='' src='https://lh3.googleusercontent.com/pw/"+ lh3 +"' type='video/mp4'></Video><span class='tl p-a bc1 c1 p-5 br-11'>Donghua Indo</span>";
